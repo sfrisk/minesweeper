a9e77c955deaa068d5bfa633101916bcfaf71444
Sarah Frisk <sarah@sarahfrisk.com>
2015-07-12 14:22:26 -0400

    Registering Events and Manipulating the Canvas Element

--- src/board.js
+++ src/board.js
@@ -1,45 +1,83 @@
 var Board = function() {
-	this.canvas = document.getElementById("minesweeper");
-	this.ctx = this.canvas.getContext("2d");
-	this.width = 400;
-	this.height = 400;
-	this.cellwidth = 50;
+	board = this;
+	board.canvas = document.getElementById("minesweeper");
+	board.ctx = this.canvas.getContext("2d");
+	board.width = 400;
+	board.height = 400;
+	board.cellwidth = 50;
+	board.cellsX = 8;
+	board.cellsY = 8;
+	board.playerMap = board.initializeArray();
+	board.minefield = board.initializeArray();
+
 
 	// this handles click events, and prevents the contextmenu from appearing
-	this.canvas.addEventListener("click", this.onClick);
-	this.canvas.addEventListener("contextmenu", this.onClick);
+	board.canvas.addEventListener("click", function(event){
+		board.onClick(event,board);
+	});
+	board.canvas.addEventListener("contextmenu", function(event){
+		board.onClick(event,board);
+	});
 }
 
 Board.prototype.draw = function() {
+	board = this;
 	this.clear();
 	this.generateGrid();
 }
 
 Board.prototype.clear = function() {
-	this.ctx.clearRect(0, 0, this.width, this.height);
+	board = this;
+	board.ctx.clearRect(0, 0, board.width, board.height);
 }
 
 Board.prototype.generateGrid = function() {
-	this.ctx.fillStyle = 'rgb(139, 137, 137)';
-	this.ctx.rect(0,0,this.width,this.height);
-	this.ctx.closePath();
-	this.ctx.fill();
+	board = this;
+	for (var x = 0; x < board.cellsY; x++){
+		for (var y = 0; y < board.cellsX; y++){
+			switch (board.playerMap[x][y]) {
+				case 0:
+					board.drawRect(x*board.cellwidth,y*board.cellwidth, board.cellwidth, board.cellwidth, [255, 140, 0]);
+					break;
+				case 2:
+					board.drawRect(x*board.cellwidth,y*board.cellwidth, board.cellwidth, board.cellwidth, [136, 136, 136]);
+					break;
+				default:
+					board.drawRect(x*board.cellwidth,y*board.cellwidth, board.cellwidth, board.cellwidth, [224, 224, 224]);
+			}
+
+		}
+	}
 }
 
+Board.prototype.drawRect = function(x,y,w,h,color) {
+	board = this;
+	board.ctx.beginPath();
+	board.ctx.fillStyle = 'rgb('+color[0]+', '+color[1]+', '+color[2]+')';
+	board.ctx.rect(x+1,y+1,w-1,h-1);
+	board.ctx.closePath();
+	board.ctx.fill();
+}
 
-Board.prototype.onClick = function(event) {
-	event.preventDefault();
-	var clickButton = "";
-	switch (event.button) {
-		case 0:
-			clickButton = "Left Click";
-			break;
-		case 2:
-			clickButton = "Right Click";
-			break;
-		default:
-			clickButton = "Unknown Click";
+Board.prototype.initializeArray = function() {
+	board = this;
+	var array = new Array(board.cellsX);
+	for (x = 0; x < board.cellsX; x++) {
+		array[x] = new Array(board.cellsY);
 	}
-	alert("x = "+event.offsetX+", y = "+event.offsetY +", " + clickButton);
+	return array;
+}
+
+Board.prototype.updatePlayerMap = function(x, y, mouseClick) {
+	board = this;
+	board.playerMap[x][y] = mouseClick;
+}
+
+Board.prototype.onClick = function(event,board) {
+	event.preventDefault();
+	var x = Math.floor(event.offsetX / board.cellwidth);
+	var y = Math.floor(event.offsetY / board.cellwidth);
+	board.updatePlayerMap(x, y, event.button);
+	board.draw();
 	return false;
 }
